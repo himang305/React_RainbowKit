a1d9791ab9d10288bd9a95b352370365fabee618
HIMANSHU GAUTAM <himang305@gmail.com>
2023-05-27 13:46:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import {Buffer} from 'buffer';
+
+window.Buffer = window.Buffer || Buffer;
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
